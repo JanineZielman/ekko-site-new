4a97b49e4bd36eb3ff19b696ff4846de1cc6c08d
Paulius <paulius@bakkenbaeck.no>
2022-08-18 00:45:46 +0300

    Add query and routes for About and Oestre pages
    Add query that fetches data for simple content pages based on their slug

---
+++ app/routes/oestre.tsx
@@ -0,0 +1,29 @@
+import type { LoaderFunction, MetaFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import Container from '~/components/container';
+import { fetchContentPage } from '~/service/data/contentPage';
+import type { PageEntry } from '~/service/data/contentPage';
+
+export const loader: LoaderFunction = () => {
+  return fetchContentPage('ostre');
+};
+
+export const meta: MetaFunction = ({ data }) => ({
+  title: (data as PageEntry).entry?.title,
+});
+
+export default function Oestre() {
+  const { entry } = useLoaderData<PageEntry>();
+
+  return (
+    <Container>
+      <div className="grid">
+        <h1>{entry.title}</h1>
+        <img src={entry.photo?.[0].url} alt={entry.title} />
+        {/* Need to do this to output rich text content */}
+        <div dangerouslySetInnerHTML={{ __html: entry?.content }} />
+      </div>
+    </Container>
+  );
+}
