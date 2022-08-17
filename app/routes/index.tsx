6eceaf215d741a005b08a05df28d4dee92f1ce02
Paulius <paulius@bakkenbaeck.no>
2022-08-17 23:24:48 +0300

    Remove `Layout` from index route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,8 @@
-import type { LoaderFunction } from '@remix-run/node';
+import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { getHomeData } from '~/service/data/home';
 
 import Container from '~/components/container';
-import Layout from '~/components/layout';
 import Spacer from '~/components/spacer';
 
 export const loader: LoaderFunction = () => {
@@ -16,42 +15,40 @@ export default function Index() {
   const { events: recentEvents } = data!;
 
   return (
-    <Layout navigation={undefined}>
-      <Container>
-        <div className="grid">
-          {recentEvents.slice(0, 2).map((item, i) => {
-            return (
-              <div key={`news-${i}`} className="item w3">
-                {item.featuredImage && (
-                  <img src={item.featuredImage[0].url} alt={item.title} />
-                )}
-                <div className="flex space-between">
-                  <div className="info">
-                    <h4>{item.title}</h4>
-                  </div>
-                  <div className="times big">{item.date}</div>
+    <Container>
+      <div className="grid">
+        {recentEvents.slice(0, 2).map((item, i) => {
+          return (
+            <div key={`news-${i}`} className="item w3">
+              {item.featuredImage && (
+                <img src={item.featuredImage[0].url} alt={item.title} />
+              )}
+              <div className="flex space-between">
+                <div className="info">
+                  <h4>{item.title}</h4>
                 </div>
+                <div className="times big">{item.date}</div>
               </div>
-            );
-          })}
-          {recentEvents.slice(2, 5).map((item, i) => {
-            return (
-              <div key={`news2-${i}`} className="item w2">
-                {item.featuredImage && (
-                  <img src={item.featuredImage[0].url} alt={item.title} />
-                )}
-                <div className="flex space-between">
-                  <div className="info">
-                    <h4>{item.title}</h4>
-                  </div>
-                  <div className="times big">{item.date}</div>
+            </div>
+          );
+        })}
+        {recentEvents.slice(2, 5).map((item, i) => {
+          return (
+            <div key={`news2-${i}`} className="item w2">
+              {item.featuredImage && (
+                <img src={item.featuredImage[0].url} alt={item.title} />
+              )}
+              <div className="flex space-between">
+                <div className="info">
+                  <h4>{item.title}</h4>
                 </div>
+                <div className="times big">{item.date}</div>
               </div>
-            );
-          })}
-          <Spacer />
-        </div>
-      </Container>
-    </Layout>
+            </div>
+          );
+        })}
+        <Spacer />
+      </div>
+    </Container>
   );
 }
